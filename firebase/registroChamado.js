71601ce1fa20ebee9e11f95477b22894ca530e26
Douglas <douglas@gmail.com>
2025-11-28 08:44:11 -0300

    Altera require do serviceAccountKey.json para /etc/secrets/serviceAccountKey.json (Render)

--- firebase/registroChamado.js
+++ firebase/registroChamado.js
@@ -3,7 +3,7 @@ const admin = require('firebase-admin');
 const fs = require('fs');
 
 // Carregar credenciais do Firebase (serviceAccountKey.json)
-const serviceAccount = require('/etc/secrets/firebase/serviceAccountKey.json');
+const serviceAccount = require('/etc/secrets/serviceAccountKey.json');
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
